005b5090eb950bf2ec79cc4db88510d1204b92c8
mawentao03 <mawentao03@baidu.com>
2018-01-24 10:43:55 +0800

    up

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 source /home/users/mawentao03/.bashrc
 python houseCheck.py > tmp
-cat tmp |mail -s "[HOUSE CHECK]" "862905339@qq.com mawentao03@baidu.com"
+cat tmp |mail -s "[HOUSE CHECK]" "wangyuyao01@baidu.com 862905339@qq.com mawentao03@baidu.com"
